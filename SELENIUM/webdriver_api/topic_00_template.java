6765f837bc7bbd74dee6ce02242237558a0fa58d
THLONG <thanhlongzz412zz@gmail.com>
2019-10-28 20:40:56 +0700

    Add testclass template

--- SELENIUM/webdriver_api/topic_00_template.java
+++ SELENIUM/webdriver_api/topic_00_template.java
@@ -1,66 +1,52 @@
 package webdriver_api;
 
+import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
-
-
 import org.openqa.selenium.By;
-
 import org.openqa.selenium.WebDriver;
-
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import org.testng.Assert;
-
 import org.testng.annotations.AfterClass;
-
 import org.testng.annotations.BeforeClass;
 
-import org.testng.annotations.Test;
+public class topic_00_template {	
+		WebDriver driver;
 
+		@BeforeClass
 
+		public void beforeClass() {
+			driver = new FirefoxDriver();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+			driver.get("");
 
-public class topic_00_template {
-	WebDriver driver;
+		}
 
-	@BeforeClass
+		@Test
 
-	public void beforeClass() {
-		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-		driver.get("");
+		public void TC_01_() {
+			driver.get("");
+		}
 
-	}
+		@Test
 
-	@Test
+		public void TC_02_() {
+			driver.get("");			
+		}
 
-	public void TC_01_() {
-		driver.get("");
-		
-	}
+		@Test
 
-	@Test
+		public void TC_03_() {
+			driver.get("");
+		}
 
-	public void TC_02_() {
-		driver.get("");	
-	}
+		@AfterClass
 
-	@Test
+		public void afterClass() {
+			driver.quit();
 
-	public void TC_03_() {
-		driver.get("");
-	}
+		}
 
-	@AfterClass
-
-	public void afterClass() {
-
-		driver.quit();
-
-	}
-
-
-
+  
 }
-
