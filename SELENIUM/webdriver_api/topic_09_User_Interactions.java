0ae0e503a878e11c21fa84065a94f818965c424b
PC <PC@172.31.99.141>
2019-12-16 11:04:28 +0700

    Topic_09_User_Interactions

--- SELENIUM/webdriver_api/topic_09_User_Interactions.java
+++ SELENIUM/webdriver_api/topic_09_User_Interactions.java
@@ -2,13 +2,21 @@ package webdriver_api;
 
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.interactions.Actions;
@@ -18,15 +26,27 @@ import org.testng.annotations.BeforeClass;
 
 public class topic_09_User_Interactions {	
 		WebDriver driver;
-		Actions action; 
+		Actions action;
+		
+		JavascriptExecutor javascriptexecutor;
+		String javascriptPath , jqueryPath;
 
 		@BeforeClass
 
 		public void beforeClass() {
-			FirefoxProfile profile = new FirefoxProfile();
+		/*	FirefoxProfile profile = new FirefoxProfile();
 			profile.setPreference("dom.webnotification.enabled" , false);
-			driver = new FirefoxDriver(profile);
+			driver = new FirefoxDriver(profile);*/
+			String projectpath = System.getProperty("user.dir");
+			System.setProperty("webdriver.chrome.driver", projectpath + "\\LIB\\chromedriver.exe");
+			driver = new  ChromeDriver();
 			action = new Actions(driver);
+			
+			javascriptexecutor = (JavascriptExecutor) driver;
+			
+			javascriptPath = projectpath + "\\DrapAndDrop\\drag_and_drop_helper.js";
+			jqueryPath = projectpath + "\\DrapAndDrop\\jquery_load_helper.js";
+					
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			driver.manage().window().maximize();
 			
@@ -108,12 +128,97 @@ public class topic_09_User_Interactions {
 			
 			String DemoText = driver.findElement(By.xpath("//p[@id='demo']")).getText();
 			Assert.assertEquals(DemoText, "Hello Automation Guys!");
+		
+		} 
+		
+		@Test
+		public void TC_05_RightClick() throws InterruptedException { // DÙNG TRÌNH DUYỆT CHROME 
+			driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
 			
+			action.contextClick(driver.findElement(By.xpath("//span[text()='right click me']"))).perform();
+			Thread.sleep(3000);
+			
+			action.moveToElement(driver.findElement(By.xpath("//span[text()='Quit']"))).perform();
+			Thread.sleep(3000);
+
+			
+			Assert.assertTrue(driver.findElement(By.xpath("//li[contains(@class,'context-menu-visible') and contains(@class,'context-menu-hover')]/span[text()='Quit']")).isDisplayed());
+			
+			action.click(driver.findElement(By.xpath("//span[text()='Quit']"))).perform();
+			Thread.sleep(3000);
+
+			
+			Assert.assertEquals(driver.switchTo().alert().getText(), "clicked: quit");
+			
+			driver.switchTo().alert().accept();
+			
+		}
+		
+		@Test
+		public void TC_06_DrapAndDrop() throws InterruptedException {
+			driver.get("https://demos.telerik.com/kendo-ui/dragdrop/angular");
+			
+			WebElement sourceCirle = driver.findElement(By.xpath("//div[@id='draggable']"));
+			WebElement targetCirle = driver.findElement(By.xpath("//div[@id='droptarget']"));
+			
+			action.dragAndDrop(sourceCirle, targetCirle).perform();
+			Thread.sleep(3000);
+			
+			Assert.assertTrue(driver.findElement(By.xpath("//div[@id='droptarget' and text()='You did great!']")).isDisplayed());
+
+		
+		} 
+		
+		
+		@Test
+		public void TC_07_DrapAndDrop_HTML5() throws InterruptedException , IOException{
+			driver.get("http://the-internet.herokuapp.com/drag_and_drop");
+			
+			String sourceCss = "#column-a";
+			String targetCss = "#column-b";
+
+			String java_script = readFile(javascriptPath);
+
+			// Inject Jquery lib to site
+			// String jqueryscript = readFile(jqueryPath);
+			// javascriptExecutor.executeScript(jqueryscript);
+
+			// A to B
+			java_script = java_script + "$(\"" + sourceCss + "\").simulateDragDrop({ dropTarget: \"" + targetCss + "\"});";
+			javascriptexecutor.executeScript(java_script);
+			Thread.sleep(3000);
+			Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-a']/header[text()='B']")).isDisplayed());
+
+			// B to A
+			javascriptexecutor.executeScript(java_script);
+			Thread.sleep(3000);
+			
+			Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-b']/header[text()='B']")).isDisplayed());
+
 			
 			
 			
 			
 		}
+		
+		public String readFile(String filePath) throws IOException {
+			Charset cs = Charset.forName("UTF-8");
+			FileInputStream stream = new FileInputStream(filePath);
+			try {
+				Reader reader = new BufferedReader(new InputStreamReader(stream, cs));
+				StringBuilder builder = new StringBuilder();
+				char[] buffer = new char[8192];
+				int read;
+				while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
+					builder.append(buffer, 0, read);
+				}
+				return builder.toString();
+			} finally {
+				stream.close();
+			}
+		}
+		
+		
 
 
 		@AfterClass
