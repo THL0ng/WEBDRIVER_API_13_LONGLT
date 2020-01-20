0461de04257b2dfc3ec0a4ad493d444e60471e7d
PC <PC@172.31.99.141>
2020-01-20 16:57:11 +0700

    Topic_14_Part6_Fluent_Wait

---
+++ SELENIUM/webdriver_api/topic_14_Wait_Part6_Fluent_Wait.java
@@ -0,0 +1,50 @@
+package webdriver_api;
+
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class topic_14_Wait_Part6_Fluent_Wait {	
+		WebDriver driver;
+
+		@BeforeClass
+
+		public void beforeClass() {
+			driver = new FirefoxDriver();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+			driver.get("");
+
+		}
+
+		@Test
+
+		public void TC_01_() {
+			driver.get("");
+		}
+
+		@Test
+
+		public void TC_02_() {
+			driver.get("");			
+		}
+
+		@Test
+
+		public void TC_03_() {
+			driver.get("");
+		}
+
+		@AfterClass
+
+		public void afterClass() {
+			driver.quit();
+
+		}
+
+  
+}
